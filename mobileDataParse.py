6c9a8ee0fec2395233ed2bd178a5e9497c2c0940
Hannah Kleidermacher <hannahkleidermacher@Hannahs-Air.hsd1.ca.comcast.net>
2018-07-04 14:41:42 -0700

    generateApps() functional

--- mobileDataParse.py
+++ mobileDataParse.py
@@ -9,41 +9,100 @@ fileName = sys.argv[1]
 file = open(fileName, 'r')
 lines = file.readlines()
 
-inXt = False
-xtMetered = False
-readingData = False
-rb = 0
-tb = 0
-timestamps = []
+class Stat:
+    rb = 0
+    tb = 0
+    timestamps = []
 
-for line in lines:
-    if 'Xt stats' in line:
-        inXt = True
-    if inXt and ('MOBILE' and 'metered=true') in line:
-        xtMetered = True
-    if xtMetered and 'st=' in line:
-        readingData = True
-        #print(line) for testing
-        # FIND rb AND tb WITHIN THE LINE & ADD RESPECTIVELY
-        rb += int(line[line.index('rb')+3:line.index('rp')-1])
-        tb += int(line[line.index('tb')+3:line.index('tp')-1])
-        # ADD TIME TO LIST OF TIMES
-        timestamps.append(int(line[line.index('st')+3:line.index('rb')-1]))
-    if readingData and 'st=' not in line:
-        break
+    def addRb(self, amt):
+        self.rb += amt
+    def addTb(self, amt):
+        self.tb += amt
+    def addTime(self, time):
+        self.timestamps.append(time)
+    def firstTime(self):
+        return time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime(self.timestamps[0]))
+    def lastTime(self):
+        return time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime(self.timestamps[-1]))
+    def dataReceived(self):
+        return str(float(self.rb) / 10**6) + " MB"
+    def dataTransmitted(self):
+        return str(float(self.tb) / 10**6) + " MB"
+    def totalData(self):
+        return str((float(self.rb + self.tb)) / 10**6) + " MB"
 
-def dataReceived():
-    return str(float(rb) / 10**6) + " MB"
-def dataTransmitted():
-    return str(float(tb) / 10**6) + " MB"
-def totalData():
-    return str((float(rb + tb)) / 10**6) + " MB"
-def firstTime():
-    return time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime(timestamps[0]))
-def lastTime():
-    return time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime(timestamps[-1]))
+class App(Stat):
+    uid = 0
+    tag = 0x0
 
-print("\n*** between " + firstTime() + " and " + lastTime() + ":")
-print("***\n*** data received: " + dataReceived())
-print("*** data tramsitted: " + dataTransmitted())
-print("*** total data: " + totalData() + "\n")
+    def __init__(self, u, t):
+        self.uid = u
+        self.tag = t
+
+stat = Stat()
+apps = []
+
+def scanDataChunk(thing, s):
+    # PASS Stat OR App; STRING TO BE SCANNED
+    # PROCESSES rb, tb, AND timestamps
+
+    thing.addRb(int(s[s.find('rb')+3:s.find('rp')-1]))
+    thing.addTb(int(s[s.find('tb')+3:s.find('tp')-1]))
+    thing.addTime(int(s[s.find('st')+3:s.find('rb')-1]))
+
+def generateStat():
+    inXt = False
+    xtMetered = False
+    readingData = False
+
+    for line in lines:
+        if 'Xt stats' in line:
+            inXt = True
+        if inXt and ('MOBILE' and 'metered=true') in line:
+            xtMetered = True
+        if xtMetered and 'st=' in line:
+            readingData = True
+            scanDataChunk(stat, line)
+        if readingData and 'st=' not in line:
+            break
+
+def grabUID(s):
+    return int(s[s.find('uid')+4:s.find('set')-1])
+
+def grabTag(s):
+    return hex(int(s[s.find('tag')+4:s.find('\n')], 16))
+
+def existingApp(a):
+    # PASS App
+    # CHECKS IF APP HAS ALREADY BEEN RECORDED
+    for app in apps:
+        if a.uid == app.uid and a.tag == app.tag:
+            return app
+    return None
+
+def generateApps():
+    inUIDTag = False
+    infoGrabbed = False
+
+    for line in lines:
+        if 'UID tag stats' in line:
+            inUIDTag = True
+        if inUIDTag and ('uid=' and 'tag=') in line:
+            currentA = App(grabUID(line), grabTag(line))
+            infoGrabbed = True
+            if existingApp(currentA) is None:
+                apps.append(currentA)
+            else:
+                currentA = existingApp(currentA)
+        if infoGrabbed and 'st=' in line:
+            scanDataChunk(currentA, line)
+        if infoGrabbed and 'type=WIFI' in line:
+            break
+
+generateApps()
+for app in apps:
+    print(str(app.rb) + " " + str(app.tb))
+'''print("\n*** between " + stat.firstTime() + " and " + stat.lastTime() + ":")
+print("***\n*** data received: " + stat.dataReceived())
+print("*** data tramsitted: " + stat.dataTransmitted())
+print("*** total data: " + stat.totalData() + "\n")'''
