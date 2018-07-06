0621bdd797d59150582531fdf393c0980767a58d
Hannah Kleidermacher <hannah.kleidermacher@gmail.com>
2018-07-05 20:38:31 -0700

    included calculation of data on a by-app basis

--- mobileDataParse.py
+++ mobileDataParse.py
@@ -10,10 +10,10 @@ file = open(fileName, 'r')
 lines = file.readlines()
 
 class Stat:
-    rb = 0
-    tb = 0
-    timestamps = []
-
+    def __init__(self):
+        self.rb = 0
+        self.tb = 0
+        self.timestamps = []
     def addRb(self, amt):
         self.rb += amt
     def addTb(self, amt):
@@ -31,19 +31,31 @@ class Stat:
     def totalData(self):
         return str((float(self.rb + self.tb)) / 10**6) + " MB"
 
-class App(Stat):
-    uid = 0
-    tag = 0x0
-
-    def __init__(self, u, t):
-        self.uid = u
+class Tag(Stat):
+    def __init__(self, t):
+        super().__init__()
         self.tag = t
+    def __str__(self):
+        return str(self.tag)
+
+class App(Stat):
+    def __init__(self, u):
+        super().__init__()
+        self.uid = u
+        self.tags = []
+    def addTag(self, Tag):
+        self.tags.append(Tag)
+    def totalTagData(self):
+        tagData = 0
+        for tag in self.tags:
+            tagData += tag.tb + tag.rb
+        return tagData
 
 stat = Stat()
 apps = []
 
 def scanDataChunk(thing, s):
-    # PASS Stat OR App; STRING TO BE SCANNED
+    # PASS ANY KIND OF Stat; STRING TO BE SCANNED
     # PROCESSES rb, tb, AND timestamps
 
     thing.addRb(int(s[s.find('rb')+3:s.find('rp')-1]))
@@ -51,14 +63,14 @@ def scanDataChunk(thing, s):
     thing.addTime(int(s[s.find('st')+3:s.find('rb')-1]))
 
 def generateStat():
-    inXt = False
+    inXtStats = False
     xtMetered = False
     readingData = False
 
     for line in lines:
         if 'Xt stats' in line:
-            inXt = True
-        if inXt and ('MOBILE' and 'metered=true') in line:
+            inXtStats = True
+        if inXtStats and ('MOBILE' and 'metered=true') in line:
             xtMetered = True
         if xtMetered and 'st=' in line:
             readingData = True
@@ -70,17 +82,51 @@ def grabUID(s):
     return int(s[s.find('uid')+4:s.find('set')-1])
 
 def grabTag(s):
-    return hex(int(s[s.find('tag')+4:s.find('\n')], 16))
+    return s[s.find('tag')+4:s.find('\n')]
 
 def existingApp(a):
-    # PASS App
-    # CHECKS IF APP HAS ALREADY BEEN RECORDED
     for app in apps:
-        if a.uid == app.uid and a.tag == app.tag:
+        if app.uid == a.uid:
             return app
     return None
 
+def existingTag(a, t):
+    # PASS App
+    # CHECKS IF App's CERTAIN Tag HAS ALREADY BEEN RECORDED
+    for tag in a.tags:
+        if tag.tag == t.tag:
+            return tag
+    return None
+
+def removeEmptyApps():
+    for app in reversed(apps):
+        if app.rb + app.tb == 0:
+            apps.remove(app)
+
 def generateApps():
+    inUIDStats = False
+    uidMetered = False
+    infoGrabbed = False
+
+    for line in lines:
+        if 'UID stats' in line:
+            inUIDStats = True
+        if inUIDStats and ('MOBILE' and 'metered=true' and 'uid=') in line:
+            uidMetered = True
+            currentUID = grabUID(line)
+            a = App(currentUID)
+            infoGrabbed = True
+
+            if existingApp(a) is None:
+                apps.append(a)
+            else:
+                a = existingApp(a)
+        if uidMetered and 'st=' in line:
+            scanDataChunk(a, line)
+        if infoGrabbed and 'type=WIFI' in line:
+            break
+
+def generateTags():
     inUIDTag = False
     infoGrabbed = False
 
@@ -88,21 +134,42 @@ def generateApps():
         if 'UID tag stats' in line:
             inUIDTag = True
         if inUIDTag and ('uid=' and 'tag=') in line:
-            currentA = App(grabUID(line), grabTag(line))
+            currentTag = Tag(grabTag(line))
+            a = existingApp(App(grabUID(line)))
             infoGrabbed = True
-            if existingApp(currentA) is None:
-                apps.append(currentA)
+
+            if existingTag(a, currentTag) is None:
+                a.addTag(currentTag)
             else:
-                currentA = existingApp(currentA)
+                currentTag = existingTag(a, currentTag)
         if infoGrabbed and 'st=' in line:
-            scanDataChunk(currentA, line)
+            scanDataChunk(currentTag, line)
         if infoGrabbed and 'type=WIFI' in line:
             break
 
+    for app in apps:
+        diff = app.tb + app.rb - app.totalTagData()
+        t = Tag("Untagged")
+        t.addRb(diff)
+        app.addTag(t)
+
+
+generateStat()
 generateApps()
+generateTags()
+removeEmptyApps()
+
+total = 0
 for app in apps:
-    print(str(app.rb) + " " + str(app.tb))
-'''print("\n*** between " + stat.firstTime() + " and " + stat.lastTime() + ":")
+    total += (app.rb + app.tb)
+    print("****** user ID " + str(app.uid) + ": " + app.totalData())
+    for tag in app.tags:
+        print("********* tag " + str(tag) + ": " + tag.totalData())
+
+    print()
+print(total)
+
+print("\n*** between " + stat.firstTime() + " and " + stat.lastTime() + ":")
 print("***\n*** data received: " + stat.dataReceived())
 print("*** data tramsitted: " + stat.dataTransmitted())
-print("*** total data: " + stat.totalData() + "\n")'''
+print("*** total data: " + stat.totalData() + "\n\n")
